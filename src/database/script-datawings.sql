0f9cb8655bb342d2e8ab7a457b37a7dde8b4145b
GustavoPimentel08 <gustavo.pcosta@sptech.school>
2025-12-09 17:29:16 -0300

    fix: alterando script banco de dados

--- src/database/script-datawings.sql
+++ src/database/script-datawings.sql
@@ -1,24 +1,14 @@
 CREATE DATABASE IF NOT EXISTS datawings;
-USE datawings; 
- 
+USE datawings;
  
 CREATE TABLE log_java (
 	id INT PRIMARY KEY auto_increment,
 	data_hora_registro DATETIME NOT NULL,
 	mensagem VARCHAR(255) NOT NULL,
 	categoria ENUM('ERRO', 'CARGA') NOT NULL
-	id INT PRIMARY KEY auto_increment,
-	data_hora_registro DATETIME NOT NULL,
-	mensagem VARCHAR(255) NOT NULL,
-	categoria ENUM('ERRO', 'CARGA') NOT NULL
-);
- 
- 
+); 
  
 CREATE TABLE empresa (
-id INT PRIMARY KEY auto_increment,
-nome_fantasia VARCHAR(255) NOT NULL,
-razao_social VARCHAR(255) UNIQUE NOT NULL,
 id INT PRIMARY KEY auto_increment,
 nome_fantasia VARCHAR(255) NOT NULL,
 razao_social VARCHAR(255) UNIQUE NOT NULL,
@@ -30,20 +20,14 @@ razao_social VARCHAR(255) UNIQUE NOT NULL,
 INSERT INTO empresa (nome_fantasia, razao_social, cnpj, status) VALUES
 ("Stefas","Stefas LTDA","12345678901234", 1);
  
- 
-
- 
 CREATE TABLE voo (
     id INT PRIMARY KEY auto_increment,
     sigla_empresa CHAR(3) NOT NULL,
-    numero_voo VARCHAR(5) NOT NULL,
     numero_voo VARCHAR(5) NOT NULL,
     codigo_autorizacao VARCHAR(2) NOT NULL,
     codigo_tipo_linha CHAR(1) NOT NULL,
     icao_origem VARCHAR(10) NOT NULL,
     icao_destino VARCHAR(10) NOT NULL,
-    uf_origem VARCHAR(50) NOT NULL,
-    uf_destino VARCHAR(50) NOT NULL,
     uf_origem VARCHAR(50) NOT NULL,
     uf_destino VARCHAR(50) NOT NULL,
     partida_prevista DATETIME NOT NULL,
@@ -52,13 +36,11 @@ CREATE TABLE voo (
     chegada_real DATETIME NOT NULL,
     tempo_atraso INT NOT NULL,
     situacao_voo ENUM('REALIZADO', 'CANCELADO') NOT NULL,
-    situacao_voo ENUM('REALIZADO', 'CANCELADO') NOT NULL,
     codigo_justificativa VARCHAR(4) NOT NULL,
     justificativa VARCHAR(200) NOT NULL,
     fk_empresa INT NOT NULL,
     FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
- 
  
 CREATE TABLE contato (
     id INT PRIMARY KEY auto_increment,
@@ -68,9 +50,19 @@ CREATE TABLE contato (
     fk_empresa INT NOT NULL,
     FOREIGN KEY (fk_empresa) REFERENCES empresa(id) ON DELETE CASCADE
 );
- 
- 
- 
+
+
+CREATE TABLE slack (
+	id INT PRIMARY KEY auto_increment,
+    status_notificacao TINYINT,
+    status_atraso TINYINT,
+    status_cancelamento TINYINT
+    descricao VARCHAR(255);
+    fk_usuario INT,
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+
  
 CREATE TABLE endereco_empresa(
     id INT PRIMARY KEY auto_increment,
@@ -99,10 +91,10 @@ CREATE TABLE codigo (
     fk_empresa INT NOT NULL,
     FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
-
  
 INSERT INTO codigo(token_ativacao, data_validade, data_criacao, status, fk_empresa) VALUES
-("ABC123", "2026-03-16", "2026-01-15 09:00:00.123", "ativo", 1);
+("ABC123", "2026-03-16", "2026-01-15 09:00:00.123", "ativo", 1),
+("DEF456", "2026-03-16", "2026-01-15 09:00:00.123", "ativo", 1);
  
 CREATE TABLE role (
     id INT PRIMARY KEY auto_increment,
@@ -110,12 +102,9 @@ CREATE TABLE role (
 );
  
  
- 
 INSERT INTO role(nome) VALUES
 ("ADMIN"),
 ("USER");
- 
-  
  
 CREATE TABLE usuario (
     id INT PRIMARY KEY auto_increment,
@@ -130,19 +119,10 @@ CREATE TABLE usuario (
     FOREIGN KEY (fk_codigo) REFERENCES codigo(id)
 );
  
+ 
 INSERT INTO usuario(nome_completo, email, senha, fk_role, fk_empresa, fk_codigo) VALUES
 ("Otavio", "otavio@gmail.com", "ta1234", 1, 1, 1);
- 
-CREATE TABLE notificacoes (
-	id INT PRIMARY KEY auto_increment,
-    mensagem VARCHAR(200) NOT NULL,
-    status TINYINT NOT NULL,
-    data_envio DATETIME NOT NULL,
-	intervalo TIME NOT NULL,
-    fk_usuario INT NOT NULL,
-    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
- 
+
  
 CREATE TABLE relatorio (
     id INT PRIMARY KEY auto_increment,
