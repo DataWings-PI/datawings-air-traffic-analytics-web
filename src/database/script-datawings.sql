3f17b8744b1630c9a3ee46b377497a6f7526a26c
GustavoPimentel08 <gustavo.pcosta@sptech.school>
2025-12-11 16:01:13 -0300

    feat: final dados

--- src/database/script-datawings.sql
+++ src/database/script-datawings.sql
@@ -1,13 +1,8 @@
 CREATE DATABASE IF NOT EXISTS datawings;
-USE datawings; 
- 
-USE datawings; 
+USE datawings;
+-- DROP DATABASE datawings;
  
 CREATE TABLE log_java (
-	id INT PRIMARY KEY auto_increment,
-	data_hora_registro DATETIME NOT NULL,
-	mensagem VARCHAR(255) NOT NULL,
-	categoria ENUM('ERRO', 'CARGA') NOT NULL
 	id INT PRIMARY KEY auto_increment,
 	data_hora_registro DATETIME NOT NULL,
 	mensagem VARCHAR(255) NOT NULL,
@@ -21,9 +16,6 @@ CREATE TABLE empresa (
 id INT PRIMARY KEY auto_increment,
 nome_fantasia VARCHAR(255) NOT NULL,
 razao_social VARCHAR(255) UNIQUE NOT NULL,
-id INT PRIMARY KEY auto_increment,
-nome_fantasia VARCHAR(255) NOT NULL,
-razao_social VARCHAR(255) UNIQUE NOT NULL,
     cnpj VARCHAR(14) NOT NULL,
     status TINYINT,
     data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -31,17 +23,12 @@ razao_social VARCHAR(255) UNIQUE NOT NULL,
  
 INSERT INTO empresa (nome_fantasia, razao_social, cnpj, status) VALUES
 ("Stefas","Stefas LTDA","12345678901234", 1);
- 
- 
- 
-INSERT INTO empresa (nome_fantasia, razao_social, cnpj, status) VALUES
-("Stefas","Stefas LTDA","12345678901234", 1);
+
  
  
 CREATE TABLE voo (
     id INT PRIMARY KEY auto_increment,
     sigla_empresa CHAR(3) NOT NULL,
-    numero_voo VARCHAR(5) NOT NULL,
     numero_voo VARCHAR(5) NOT NULL,
     codigo_autorizacao VARCHAR(2) NOT NULL,
     codigo_tipo_linha CHAR(1) NOT NULL,
@@ -49,14 +36,11 @@ CREATE TABLE voo (
     icao_destino VARCHAR(10) NOT NULL,
     uf_origem VARCHAR(50) NOT NULL,
     uf_destino VARCHAR(50) NOT NULL,
-    uf_origem VARCHAR(50) NOT NULL,
-    uf_destino VARCHAR(50) NOT NULL,
     partida_prevista DATETIME NOT NULL,
     partida_real DATETIME NOT NULL,
     chegada_prevista DATETIME NOT NULL,
     chegada_real DATETIME NOT NULL,
     tempo_atraso INT NOT NULL,
-    situacao_voo ENUM('REALIZADO', 'CANCELADO') NOT NULL,
     situacao_voo ENUM('REALIZADO', 'CANCELADO') NOT NULL,
     codigo_justificativa VARCHAR(4) NOT NULL,
     justificativa VARCHAR(200) NOT NULL,
@@ -75,7 +59,9 @@ CREATE TABLE contato (
 );
  
  
- 
+ INSERT INTO contato (departamento, numero, tipo_numero, fk_empresa)
+VALUES 
+('Financeiro', '1133224455', 'fixo', 1);
  
 CREATE TABLE endereco_empresa(
     id INT PRIMARY KEY auto_increment,
@@ -97,9 +83,6 @@ INSERT INTO endereco_empresa(apelido, logradouro, numero, complemento, bairro, c
  
  
  
-INSERT INTO endereco_empresa(apelido, logradouro, numero, complemento, bairro, cep, cidade, uf, fk_empresa) VALUES
-("Trabalho","Travessa José Marciano Pontes", "16","arvore na frente da casa","barro branco", "08473573","São Paulo", "SP", 1);
- 
 CREATE TABLE codigo (
     id INT PRIMARY KEY auto_increment,
     token_ativacao CHAR(6) NOT NULL,
@@ -107,15 +90,12 @@ CREATE TABLE codigo (
     data_criacao DATETIME NOT NULL,
     status ENUM('ativo', 'expirado') NOT NULL,
     fk_empresa INT NOT NULL,
-    FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    FOREIGN KEY (fk_empresa) REFERENCES empresa(id) ON DELETE CASCADE
 );
  
 INSERT INTO codigo(token_ativacao, data_validade, data_criacao, status, fk_empresa) VALUES
 ("ABC123", "2026-03-16", "2026-01-15 09:00:00.123", "ativo", 1);
  
- 
-INSERT INTO codigo(token_ativacao, data_validade, data_criacao, status, fk_empresa) VALUES
-("ABC123", "2026-03-16", "2026-01-15 09:00:00.123", "ativo", 1);
  
 CREATE TABLE role (
     id INT PRIMARY KEY auto_increment,
@@ -128,13 +108,6 @@ INSERT INTO role(nome) VALUES
 ("USER");
  
   
- 
- 
- 
-INSERT INTO role(nome) VALUES
-("ADMIN"),
-("USER");
- 
   
  
 CREATE TABLE usuario (
@@ -145,19 +118,15 @@ CREATE TABLE usuario (
     fk_role INT NOT NULL,
     fk_empresa INT NOT NULL,
     fk_codigo INT NOT NULL,
-    FOREIGN KEY (fk_empresa) REFERENCES empresa(id),
-    FOREIGN KEY (fk_role) REFERENCES role(id),
-    FOREIGN KEY (fk_codigo) REFERENCES codigo(id)
+    FOREIGN KEY (fk_empresa) REFERENCES empresa(id) ON DELETE CASCADE,
+    FOREIGN KEY (fk_role) REFERENCES role(id)ON DELETE CASCADE,
+    FOREIGN KEY (fk_codigo) REFERENCES codigo(id) ON DELETE CASCADE
 );
  
  
 INSERT INTO usuario(nome_completo, email, senha, fk_role, fk_empresa, fk_codigo) VALUES
 ("Otavio", "otavio@gmail.com", "ta1234", 1, 1, 1);
  
- 
- 
-INSERT INTO usuario(nome_completo, email, senha, fk_role, fk_empresa, fk_codigo) VALUES
-("Otavio", "otavio@gmail.com", "ta1234", 1, 1, 1);
  
 CREATE TABLE notificacoes (
 	id INT PRIMARY KEY auto_increment,
